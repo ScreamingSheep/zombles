98b7bf4e26da4d648ed21fe9ba5bfc34f10505ce
Rishi Ramraj <rishi@bestpremise.com>
2012-11-06 19:01:39 -0500

    Added other parts of the data model.

--- bootstrap.sql
+++ bootstrap.sql
@@ -15,3 +15,36 @@ CREATE TABLE rooms (
     top_wall int NOT NULL,
     bottom_wall int NOT NULL
 );
+
+-- Doors.
+CREATE TABLE doors (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    room_id int NOT NULL REFERENCES rooms,
+    wall ENUM('left', 'right', 'top', 'bottom') NOT NULL,
+    position int NOT NULL
+);
+
+-- Treasures.
+CREATE TABLE treasures (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    room_id int NOT NULL REFERENCES rooms,
+    name text NOT NULL,
+    x int NOT NULL,
+    y int NOT NULL
+);
+
+-- Users.
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name text NOT NULL,
+    x int NOT NULL,
+    y int NOT NULL,
+    health int NOT NULL
+);
+
+-- Items.
+CREATE TABLE items (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id int NOT NULL REFERENCES rooms,
+    name text NOT NULL
+);
